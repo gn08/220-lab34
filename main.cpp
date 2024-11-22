5abf2f7c20417e46f671ce6adff61fcde87e9e04
gn08 <gabbyfilipova@gmail.com>
2024-11-21 16:29:30 -0800

    commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Graph{
+private:
+    int vertices;
+    vector<list<pair<int, int>>> adjacenyList;
+    
+public:
+
+}
+
+int main(){
+
+}
