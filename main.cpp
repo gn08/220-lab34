2c000906c79d9cebac448dbb89cea31df4c76fdd
gn08 <gabbyfilipova@gmail.com>
2024-11-22 13:05:01 -0800

    commit 1:05

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 #include <list>
 
 using namespace std;
 
 class Graph{
 private:
-    int vertices;
-    vector<list<pair<int, int>>> adjacenyList;
+    unordered_map<int, vector<pair<int, int>>> adjList;
 
 public:
-    Graph(int v): vertices(v){
-        adjacenyList.resive(vertices);
+    void addEdge(int u, int v, int weight){
+        adjList[u].emplace_back(v, weight);
+        adjList[v].emplace_back(u, weight);
+    }
+
+    void pointGraph() const{
+        
     }
 }
 
